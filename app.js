083daa5c78d875df1d98d9f7c4ded2ee6246b103
ozzkan-burak <brk.ozzkan@gmail.com>
2023-01-09 21:52:11 +0300

    episode4

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ const express = require("express");
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// import routes
+const userRoutes = require('./routes/user');
+ 
 // app
 const  app = express();
 
 // db
 mongoose.connect(process.env.MONGO_URI,{
   useNewUrlParser: true,
-  useCreateIndex:true
-})
+}).then(()=> console.log('DB Connected'));
 
-// routes
-app.get("/", (req, res)=> {
-  res.send("hello from node")
-});
+// routes middlewares
+app.use(userRoutes);
 
 const PORT = process.env.PORT || 8000;
 
