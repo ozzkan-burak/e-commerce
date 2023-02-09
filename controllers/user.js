b9630c49ecae6662184774e89a11cbdd8333aaf8
ozzkan-burak <brk.ozzkan@gmail.com>
2023-02-09 23:38:08 +0300

    epsode15

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,17 @@
 const User = require("../models/user");
+const {errorHandler} = require('../helpers/dbErrorHandler');
 
 // exports.sayHi = (req, res) => {
 //   res.json({message: "hello there"})
 // }
 
 exports.signup = (req, res) => {
+  console.log('REQUEST_BODY', req.body)
   const user = new User(req.body);
   user.save((err, user) => {
     if(err) {
       return res.status(400).json({
-        err
+        err: errorHandler(err);
       })
     }
     res.json({
